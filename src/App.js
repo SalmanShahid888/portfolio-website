9b47791a8be654f025d7afeb06ff92af760f6d96
salman8896 <salmanshahid8896@gmail.com>
2022-10-08 18:23:10 +0500

    Added Introduction section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Introduction from './Introduction'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Introduction/>
     </div>
   );
 }
