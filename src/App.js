f6b2ff5fc8bb6fdd9da984cba71617d51b9f314e
salman8896 <salmanshahid8896@gmail.com>
2022-10-09 19:50:33 +0500

    Added Skills and about section, About section needs some work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import About from './About';
 import './App.css';
 import Header from './Header';
 import Introduction from './Introduction'
+import Skills from './Skills';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Introduction/>
+      <Skills/>
+      <About/>
     </div>
   );
 }
