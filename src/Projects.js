360ed83ce6a05560522421fc83aaa3a1c37aa3cc
salman8896 <salmanshahid8896@gmail.com>
2022-10-13 22:34:45 +0500

    Added Project Section, Projects need to be added.

---
+++ src/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Projects.module.css'
+
+const Projects = () => {
+  return (
+    <>
+    <div className={styles.Projects__section}>Projects</div>
+    </>
+  )
+}
+
+export default Projects
