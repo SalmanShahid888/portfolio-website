4d7c279e7ab063ec2bb142fad84a56cdf6f45b0f
salman8896 <salmanshahid8896@gmail.com>
2022-10-27 16:59:21 +0500

    added Email key file

---
+++ src/EmailKey.js
@@ -0,0 +1,9 @@
+
+const KEY = {
+    serviceID: "service_ixmpzjn",
+    templateID: "template_ncdbot5",
+    userID: "ISwKylpLqm-6UT4vc",
+    privateID: "RAUVVWIkzJnuSGM2RM-rl"
+}
+
+export default KEY
